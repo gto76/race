738e64a71f2ea78f40619d45ace2ff31048c8497
Jure Sorn <sornjure@gmail.com>
2014-08-28 23:12:40 +0200

    now it acts as primitive drawing program

--- term.c
+++ term.c
@@ -25,22 +25,87 @@ void set_input_mode(void) {
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &tattr);
 }
 
-int main(void) {
-	char c;
-	printf("Setting input mode.\n");
-	set_input_mode();
-	printf("Input mode set.\n");
+int const DEBUG = 0;
+int const numOfPlayers = 1;
 
-	while(1) {
-		read(STDIN_FILENO, &c, 1);
-		if (c == '\004') {
-			break;
-		} else if (c == 't') {
-			break;	
-		} else {
-			putchar(c);
+struct player {
+	int x, y;
+	char c;
+	int dir[4]; // charcode of all directions
+};
+typedef struct player PLAYER;
+
+int checkMove(char c, PLAYER (*p)[]) {
+	if (DEBUG) printf("\033[%d;%dHkey: %d\n", 1, 1, c);
+	int i, j;
+	for (i = 0; i < numOfPlayers; i++) {
+		for (j = 0; j < 4; j++) {
+			if ((*p)[i].dir[j] == c) {
+				movePlayer(&((*p)[i]), j);
+				if (DEBUG) printf("\033[%d;%dHdirection: %d\n", 2, 1, j);
+				if (DEBUG) printf("\033[%d;%dHplayer: %d\n", 3, 1, i);
+				return 0;
+			}
 		}
 	}
+}
+enum direction {UP, DOWN, RIGHT, LEFT};
+
+int main(void) {
+	clearScreen();
+	set_input_mode();
+
+	PLAYER p[numOfPlayers];
+	p[0].x = 10;
+	p[0].y = 10;
+	p[0].c = '1';
+	p[0].dir[0] = 65; // up
+	p[0].dir[1] = 66; // down
+	p[0].dir[2] = 67; // right
+	p[0].dir[3] = 68; // left
+
+	char c;
+	while(1) {
+		c = getc(stdin);
+		if (c == '\004') {
+			break;
+		} else {
+			checkMove(c, &p);
+			printPlayer(&p[0]); 
+		}
+	}
+	clearScreen();
 	return EXIT_SUCCESS;
 }	
 
+int movePlayer(PLAYER *p, int dir) {
+	if (DEBUG) printf("\033[%d;%dHMoving player %c\n", 4, 1, (*p).c);
+	//erasePlayer(*p);
+	switch (dir) {
+		case UP:
+			(*p).y--;
+			break;
+		case DOWN:
+			(*p).y++;
+			break;
+		case RIGHT:
+			(*p).x++;
+			break;
+		case LEFT:
+			(*p).x--;
+			break;
+	}
+	printPlayer(p);
+}
+
+int erasePlayer(PLAYER *p) {
+	printf("\033[%d;%dH%c\n", (*p).y, (*p).x, ' ');
+}
+
+int printPlayer(PLAYER *p) {
+	printf("\033[%d;%dH%c\n", (*p).y, (*p).x, (*p).c);
+}
+
+int clearScreen(void) {
+	printf("\e[1;1H\e[2J");
+}
